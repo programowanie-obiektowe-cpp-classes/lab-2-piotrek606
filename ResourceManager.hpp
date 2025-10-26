cfa286e21dda0bfe3093523ac4ed8947f9d08b08
piotrek606 <150963313+piotrek606@users.noreply.github.com>
2025-10-26 19:18:45 +0100

    dodano ResourceManager

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,31 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager() : p_(new Resource()) {}
+
+    ~ResourceManager()
+    {
+        delete p_;
+        p_ = nullptr;
+    }
+
+    ResourceManager(const ResourceManager& other) : p_(new Resource(*other.p_)) {}
+
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            Resource* nowy = new Resource(*other.p_);
+            delete p_;
+            p_ = nowy;
+        }
+        return *this;
+    }
+
+    double get() {
+        return (*p_).get(); 
+    }
+
+private:
+    Resource* p_;
 };
